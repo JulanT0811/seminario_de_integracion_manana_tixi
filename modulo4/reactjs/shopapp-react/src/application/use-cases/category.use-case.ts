084e39356b8c8d2819626830dd70dd2dd3e1cae2
JulanT0811 <jtlengua.9b@gmail.com>
2026-07-10 09:51:56 -0500

    SHOPAPP WEB CHECKPOINT 09 - DASHBOARD KPI - 10-07-2026

--- modulo4/reactjs/shopapp-react/src/application/use-cases/category.use-case.ts
+++ modulo4/reactjs/shopapp-react/src/application/use-cases/category.use-case.ts
@@ -1,15 +1,16 @@
 // src/application/use-cases/category.use-case.ts
 import type { CategoryRepository } from '@/domain/ports/category.repository'
 import type { Category } from '@/domain/entities/category.entity'
+import type { CategoryStats } from '@/domain/entities/category-stats.entity'
 
 export class CategoryUseCase {
-    private readonly categoryRepository: CategoryRepository
+  constructor(private readonly categoryRepository: CategoryRepository) {}
 
-    constructor(categoryRepository: CategoryRepository) {
-        this.categoryRepository = categoryRepository
-    }
+  getCategories(): Promise<Category[]> {
+    return this.categoryRepository.getCategories()
+  }
 
-    getCategories(): Promise<Category[]> {
-        return this.categoryRepository.getCategories()
-    }
+  getStats(): Promise<CategoryStats> {
+    return this.categoryRepository.getStats()
+  }
 }
