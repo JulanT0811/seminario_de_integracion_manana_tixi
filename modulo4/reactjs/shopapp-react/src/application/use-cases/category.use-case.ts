51edcefd3cda6ea395230467e1eb066e57e2d0ed
JulanT0811 <jtlengua.9b@gmail.com>
2026-07-10 10:01:12 -0500

    SHOPAPP WEB CHECKPOINT 10 - ADMIN CATEGORIES - 10-07-2026

--- modulo4/reactjs/shopapp-react/src/application/use-cases/category.use-case.ts
+++ modulo4/reactjs/shopapp-react/src/application/use-cases/category.use-case.ts
@@ -2,6 +2,8 @@
 import type { CategoryRepository } from '@/domain/ports/category.repository'
 import type { Category } from '@/domain/entities/category.entity'
 import type { CategoryStats } from '@/domain/entities/category-stats.entity'
+import type { CreateCategoryDto } from '@/application/dtos/create-category.dto'
+import type { UpdateCategoryDto } from '@/application/dtos/update-category.dto'
 
 export class CategoryUseCase {
   constructor(private readonly categoryRepository: CategoryRepository) {}
@@ -12,5 +14,17 @@ export class CategoryUseCase {
 
   getStats(): Promise<CategoryStats> {
     return this.categoryRepository.getStats()
+  }
+
+  createCategory(dto: CreateCategoryDto): Promise<Category> {
+    return this.categoryRepository.createCategory(dto)
+  }
+
+  updateCategory(id: number, dto: UpdateCategoryDto): Promise<Category> {
+    return this.categoryRepository.updateCategory(id, dto)
+  }
+
+  deleteCategory(id: number): Promise<void> {
+    return this.categoryRepository.deleteCategory(id)
   }
 }
